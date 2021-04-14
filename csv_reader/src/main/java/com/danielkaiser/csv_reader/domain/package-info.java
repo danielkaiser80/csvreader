01c27f3579a78143e5d09b2024eb2077da73b371
Daniel Kaiser <d.kaiser@wealthpilot.de>
2021-04-14 12:04:00 +0200

    Raise some versions.

---
+++ csv_reader/src/main/java/com/danielkaiser/csv_reader/domain/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.danielkaiser.csv_reader.domain;
+
+import org.springframework.lang.NonNullApi;
---
+++ csv_reader/src/main/java/com/danielkaiser/csv_reader/repository/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.danielkaiser.csv_reader.repository;
+
+import org.springframework.lang.NonNullApi;
